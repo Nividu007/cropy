f92837212af97da86494d3c25464d6e4c95bd359
Nividu007 <147181356+Nividu007@users.noreply.github.com>
2025-04-05 00:55:46 +0530

    1.0

--- src/components/Use.jsx
+++ src/components/Use.jsx
@@ -4,8 +4,8 @@ const Use = () => {
   return (
     <div id="how" className="flex mx-10 items-top justify-center mb-10 ">
       <div className="w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 shadow-[7px_6px_14px_7px_rgba(0,_0,_0,_0.35)] p-6">
-        <h2 className="mb-4 text-2xl font-semibold text-gray-800 dark:text-gray-100">How to use</h2>
-        <p className="text-gray-600 dark:text-gray-300 leading-relaxed">
+        <h2 className="mb-4 text-2xl font-semibold text-gray-100">How to use</h2>
+        <p className="text-gray-300 leading-relaxed">
           Enter Your Data Into the Form: Fill in the required fields with the relevant data<br />Click Predict Yield<br />Yep That's it!
         </p>
       </div>
